602cdd0e5c1eef033e7626f6d4389d7d0c37d6e8
DennisLiu <dennis.liu@isturay.com>
2015-08-31 13:38:27 -0700

    update func names

--- PeakDensity.py
+++ PeakDensity.py
@@ -6,7 +6,7 @@ import numpy as np
 from sklearn import manifold
 
 
-def localDensity(dist, dc, gaussian=False):
+def local_density(dist, dc, gaussian=False):
     if gaussian:
         rho = np.sum(np.exp(-(dist/dc)**2), 0) - 1
     else:
@@ -14,7 +14,7 @@ def localDensity(dist, dc, gaussian=False):
     return rho
 
 
-def distanceToPeak(dist, rho):
+def distance_peak(dist, rho):
     n = len(rho)
     delta = np.zeros(n)
     for i in range(n):
@@ -26,69 +26,62 @@ def distanceToPeak(dist, rho):
     return delta
 
 
-def estimateDc(dist, low=0.01, high=0.02):
+def estimate_dc(dist, low=0.01, high=0.02):
     dc = np.min(dist)
-    dcMod = np.median(dist[np.triu_indices(len(dist), 1)]) * 0.01
+    dc_mod = np.median(dist[np.triu_indices(len(dist), 1)]) * 0.01
     n = len(dist)
 
     while True:
-        neighRate = np.mean((np.sum(dist < dc, 0) - 1) / float(n))
-        if low < neighRate < high:
+        neigh_rate = np.mean((np.sum(dist < dc, 0) - 1) / float(n))
+        if low < neigh_rate < high:
             break
         else:
-            if neighRate > high:
-                dc -= dcMod
-                dcMod /= 2
-            dc += dcMod
+            if neigh_rate > high:
+                dc -= dc_mod
+                dc_mod /= 2
+            dc += dc_mod
     return dc
 
-def densityCluster(dist, gaussian=False):
-    dc = estimateDc(dist)
-    rho = localDensity(dist, dc, gaussian=gaussian)
-    delta = distanceToPeak(dist, rho)
-    densityDelta = pd.DataFrame({'rho': pd.Series(rho), 'delta': pd.Series(delta)})
-    return densityDelta
-
-def Kclusters(densityDelta, K):
-    pos = np.argmax(densityDelta.rho)
-    peak = densityDelta.values[pos]
-    d = []
-    for v in densityDelta.values:
-        d.append(np.sqrt(np.sum((v - peak) ** 2)))
-    poscut = np.argsort(d)[K+1]
-    return densityDelta.rho[poscut], densityDelta.delta[poscut]
+def density_cluster(dist, gaussian=False):
+    dc = estimate_dc(dist)
+    rho = local_density(dist, dc, gaussian=gaussian)
+    delta = distance_peak(dist, rho)
+    info = pd.DataFrame({'rho': pd.Series(rho), 'delta': pd.Series(delta)})
+    return info
 
 
-def findClusters(dist, densityDelta, rho, delta):
-    peaks = np.where([x and y for x, y in zip(densityDelta['rho'] > rho, densityDelta['delta'] > delta)])[0]
-    n = len(dist)
-    runorder = np.argsort(densityDelta.rho)[::-1]
-    cluster = np.zeros(n)
-    for i in runorder:
-        if i in peaks:
-            cluster[i] = list(peaks).index(i)
+def find_clusters(dist, info, rho, delta):
+    peaks = np.where([x and y for x, y in zip(info['rho'] > rho, info['delta'] > delta)])[0]
+    run_order = list(np.argsort(info.rho)[::-1])
+    cluster = np.zeros(len(dist))
+    for run in run_order:
+        if run in peaks:
+            cluster[run] = list(peaks).index(run)
         else:
-            higher = list(np.where(densityDelta.rho > densityDelta.rho[i])[0])
-            cluster[i] = cluster[higher[np.argmin(dist[i, higher])]]
+            higher = list(np.where(info.rho > info.rho[run])[0])
+            cluster[run] = cluster[higher[np.argmin(dist[run, higher])]]
     return cluster
 
 
 if __name__ == '__main__':
-    rawData = pd.read_csv('./Data/iris.dat')
-    data = rawData.iloc[:, 0:4]
+    raw_data = pd.read_csv('./Data/iris.dat')
+    data = raw_data.iloc[:, 0:4]
     n = data.shape[0]
-    dist = np.zeros((n, n))
+    distance = np.zeros((n, n))
     for i in range(n):
         for j in range(i + 1, n):
-            dist[i, j] = np.sqrt(np.sum((np.array(data.iloc[i, :]) - np.array(data.iloc[j, :])) ** 2))
-            dist[j, i] = dist[i, j]
-    res = densityCluster(dist, gaussian=False)
+            distance[i, j] = np.sqrt(np.sum((np.array(data.iloc[i, :]) - np.array(data.iloc[j, :])) ** 2))
+            distance[j, i] = distance[i, j]
+    res = density_cluster(distance, gaussian=False)
+
 
     print ggplot(aes(x='rho', y='delta'), data=res) + geom_point() + ggtitle('decision graph')
 
+    cls = find_clusters(distance, res, np.percentile(res.rho, 80), np.percentile(res.delta, 80))
 
-    cluster = findClusters(dist, res, np.percentile(res.rho, 80), np.percentile(res.delta, 80))
     mds = manifold.MDS(dissimilarity="precomputed")
-    coords = mds.fit(dist).embedding_
-    points = pd.DataFrame({'x': pd.Series(coords[:, 0]), 'y': pd.Series(coords[:, 1]), 'cluster': pd.Series(cluster)})
+    coord = mds.fit(distance).embedding_
+    points = pd.DataFrame({'x': pd.Series(coord[:, 0]), 'y': pd.Series(coord[:, 1]), 'cluster': pd.Series(cls)})
+
     print ggplot(aes(x='x', y='y', color='cluster'), data=points) + geom_point()
+
